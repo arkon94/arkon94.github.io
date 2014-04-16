2b4a5d6b8e2734c851f39dca68581421f2eec05f
Eugene Cheung <ey.cheung@outlook.com>
2014-04-16 19:55:35 -0400

    Arrows and stuff.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,9 @@
-// Smooth scrollTo
+/* Smooth scrollTo */
 var scrollTo = function(el, off){
   $('html, body').animate({
     scrollTop: $(el).offset().top - off
   }, 650);
 }
 
-$("#top").click(function(){ scrollTo("body", 0); });
-
-// Sticky nav
-// var scrollTop;
-
-// var stickyNav = function(){
-//   scrollTop = $(window).scrollTop();
-
-//   if (scrollTop > 285) {
-//     $('nav').addClass('sticky');
-//   } else {
-//     $('nav').removeClass('sticky');
-//   }
-// };
-
-// stickyNav();
-// $(window).scroll(function(){ stickyNav(); });
-// window.onresize = function(){ stickyNav(); }
+$("#header-more").click(function(){ scrollTo("#about", 0); });
+$("#footer-top").click(function(){ scrollTo("body", 0); });
