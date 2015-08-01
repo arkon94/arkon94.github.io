9fa3466a7d4e0dcbe7b7b6ac5170e3b007308320
Eugene Cheung <echeung@infusion.com>
2015-07-31 23:39:56 -0400

    Fixed mobile header, changed header links, tweak grid/footer

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function scrollTo (element, to, duration) {
+    if (duration <= 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function() {
+        if (element.scrollTop >= to) return;
+        element.scrollTop = element.scrollTop + perTick;
+        scrollTo(element, to, duration - 10);
+    }, 10);
+}
+
+var links = document.querySelectorAll('[href^="#"]');
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function (e) {
+        e.preventDefault();
+
+        var link = this.getAttribute('href').substr(1);
+
+        scrollTo(document.body, document.getElementById(link).offsetTop, 350);
+    }, false);
+}
